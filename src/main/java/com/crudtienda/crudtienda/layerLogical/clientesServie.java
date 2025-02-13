38a7a8b7e501265f3d770f8c9bbe46e2db9c807b
ivan ayala <saitoani@hotmail.com>
2025-02-13 00:39:37 -0600

    se cambio el paquete de la capa de servicio

--- src/main/java/com/crudtienda/crudtienda/layerLogical/clientesServie.java
+++ src/main/java/com/crudtienda/crudtienda/layerLogical/clientesServie.java
@@ -1,4 +1,4 @@
-package com.crudtienda.layerLogical;
+package com.crudtienda.crudtienda.layerLogical;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
